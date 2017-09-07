428128a0f09b1e5e4854f69862f982c21d36dbc4
zhoufeng01 <zf1125828>
2017-09-07 20:49:24 +0800

    更新作业

--- PythonCase/Frame_new.py
+++ PythonCase/Frame_new.py
@@ -5,41 +5,6 @@ import xlrd
 import re
 import os
 
-def work_sheet():
-        workbook = xlwt.Workbook(encoding='utf8')
-        worksheet = workbook.add_sheet('Frame')
-        j = 0
-        for File_list in dir_list(Local):
-            print File_list
-
-        # with open('/Users/zhoufeng/Desktop/帧率.txt','r+') as fp:
-        #     i = 0
-        #     list_line=len(fp.readlines())
-        #     fp.seek(0, 0)
-        #     for lint_count in range(list_line):
-        #         Line=fp.readline()
-        #         strA='GoodsManagementActivity'
-        #         if Line.find('ui.activity.GoodsManagementActivity')!=-1:
-        #             #用acitivity名称填充第一行
-        #             worksheet.write(0,j,strA)
-        #             #在获取一行内容
-        #             Line = fp.readline()
-        #             if Line.find('Execute')!=-1:
-        #                 line = fp.readline()
-        #                 while 1:
-        #                     line = fp.readline()
-        #                     if line.strip()!='':
-        #                         list=re.split(r'\s',line.strip())
-        #                         total = float(list[0]) + float(list[1]) + float(list[2])
-        #                         print total
-        #                         i = i + 1
-        #                         print i
-        #                         worksheet.write(i,0,total)
-        #
-        #                     else:
-        #                         break
-        #     workbook.save('/Users/zhoufeng/Desktop/数据.xls')
-
 
 def dir_list(Local):
     file_list=[]
@@ -53,8 +18,55 @@ def dir_list(Local):
                 file_list.append(Filename)
     return file_list
 
-Local='/Users/zhoufeng/Desktop/Frame'
-work_sheet()
+
+
+def work_save_excle(Local):
+    workbook = xlwt.Workbook(encoding='utf8')
+    worksheet = workbook.add_sheet('Frame')
+    i = 0
+    j = 1
+    for File_list in dir_list(Local):
+        print File_list
+        with open(File_list, 'r+') as fp:
+            list_line = len(fp.readlines())
+            fp.seek(0, 0)
+            for lint_count in range(list_line):
+                Line = fp.readline()
+                #先找到具体数据的标志符号
+                if Line.__contains__(".ui.activity"):
+                    #正则，生成匹配结果的list
+                    pattern = re.findall("Activity/android", Line.strip())
+                    print pattern
+                    #判断是否该元素是否为需要的元素
+    #                 if pattern[0] == 'MainActivity':
+    #                     print '数据不做处理'
+    #                 else:
+    #                     #在获取目标行后面的一行内容
+    #                     Line = fp.readline()
+    #                     ##判断是否下一行的数据是否为：
+    #                     if Line.strip().find("Execute")!=-1:
+    #                         print("不做处理")
+    #                         # 用acitivity名称填充第一行
+    #                         worksheet.write(0, i, pattern[0])
+    #                         i += 1
+    #                         line = fp.readline()
+    #                         print line
+    #                         while 1:
+    #                             line = fp.readline()
+    #                             if line.strip()!='':
+    #                                 list=re.split(r'\s',line.strip())
+    #                                 total = float(list[0]) + float(list[1]) + float(list[2])
+    #                                 worksheet.write(j,i-1,total)
+    #                                 j += 1
+    #                             else:
+    #                                 j = 1
+    #                                 break
+    #
+    # workbook.save(os.path.join(Local, '数据.xls'))
+
+
+LocalA='/Users/zhoufeng/Desktop/Frame'
+work_save_excle(LocalA)
 
 
 
